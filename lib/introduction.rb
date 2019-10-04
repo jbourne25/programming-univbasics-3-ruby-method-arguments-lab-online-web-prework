d223467251a3e411b00c5077f6db5efc7b003d70
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-04 01:53:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@
 NAME = "FRED"
 "HI, MY NAME IS #{NAME}."
 
-#introduction_with_language"FRED", "RUBY"
+LANGUAGE = "RUBY"
 "HI, MY NAME IS #{NAME} AND I AM LEARNING TO PROGRAM IN #{LANGUAGE}."
 
 #introduction_with_language_optional('FRED', 'JAVA' = "RUBY")

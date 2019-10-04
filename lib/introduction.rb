02c9b505dd13ae2eed895ffdd08a67d657a3d591
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-04 01:25:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,10 @@ NAME = "FRED"
 "HI, MY NAME IS #{NAME}."
 
 introduction_with_language = "NAME", "LANGUAGE"
+NAME = "DAN"
+LANGUAGE = "EMBER"
 "HI, MY NAME IS #{NAME} AND I AM LEARNING TO PROGRAM IN #{LANGUAGE}."
+
 
 
 
